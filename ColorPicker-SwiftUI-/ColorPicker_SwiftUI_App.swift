1a4e778765c0f5331c798356984998a61e4d5270
ddyack <ddyack@yandex.ru>
2020-10-19 21:49:48 +0300

    Initial Commit

---
+++ ColorPicker-SwiftUI-/ColorPicker_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPicker_SwiftUI_App.swift
+//  ColorPicker-SwiftUI-
+//
+//  Created by ddyack on 19.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct ColorPicker_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
